5328967eb887318c1b07c2f644499b7de50fa967
dror <drorputre@gmail.com>
2025-08-13 20:38:13 +0300

    edit random question

--- script.js
+++ script.js
@@ -93,6 +93,14 @@ const questions = [
 let currentQuestion = 0;
 
 let answered= false;
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+shuffleArray(questions); 
 
 function showQuestion() {
   const q = questions[currentQuestion];
